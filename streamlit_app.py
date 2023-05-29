6c48c5a74829f6f6a52488ab30986ce0afb2bf2d
Atin Sakkeer Hussain <atinsaki@gmail.com>
2023-05-29 15:05:25 +0800

    Image Captioning

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,15 @@
 import streamlit as st
+from PIL import Image
+from transformers import pipeline
 
-st.title('Object Detection App')
 
-st.camera_input("Realtime Object Detection")
+st.title('Image Captioning App')
+
+uploaded_file = st.file_uploader("Upload Image")
+image = Image.open(uploaded_file)
+
+image_to_text = pipeline("image-to-text", model="nlpconnect/vit-gpt2-image-captioning")
+
+caption = image_to_text("https://ankur3107.github.io/assets/images/image-captioning-example.png")
+
+st.write(caption)
