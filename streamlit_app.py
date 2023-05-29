9dd4e7d43af9da7410d9eee34ff690dd5461ebba
Atin Sakkeer Hussain <atinsaki@gmail.com>
2023-05-29 15:35:17 +0800

    Image Captioning

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,15 +2,15 @@ import streamlit as st
 from PIL import Image
 from transformers import BlipProcessor, BlipForConditionalGeneration
 
-processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
-model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
-
 
 st.title('Image Captioning App')
 
 uploaded_file = st.file_uploader("Upload Image")
 
 if uploaded_file is not None:
+    processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
+    model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
+
     image = Image.open(uploaded_file)
 
     inputs = processor(image, return_tensors="pt")
